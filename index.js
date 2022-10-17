2e8fa674f88a75cd09df8e6f3a2d867ebcfba7d4
Karen Vargas <kalivargas.11@gmail.com>
2022-10-17 01:03:57 -0500

    Casi termianr la pagina Carrera, comentar y ordenar

--- index.js
+++ index.js
@@ -6,19 +6,15 @@ const port = 3000
 const path = require('path') 
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
-//middleware para los archivos staticos
-app.use('/static',express.static('public'))
-
-
 //Establecer el motor que se va a usar
 app.set('view engine', 'ejs')
 //Donde van a estar guardadas las vistas
 app.set('views', __dirname + '/views')
 
-//El establecimiento de las rutas basicas que tiene un proyecto
+
+//El establecimiento de las rutas basicas que tiene eñ proyecto
 
 app.get('/', (req, res) => {
- // res.send('Pagina Principal')
   console.log(__dirname)
   res.render('index')
 })
@@ -32,14 +28,13 @@ app.get('/pasatiempos', (req, res) => {
   res.render('pasatiempos')
 })
 
+
 //Establecer la ruta al error
-//Cada vez que se presente un 404 lo va redirigir al archivo creado 404.html
 app.use((req, res, next) =>{
       res.status(404).sendFile(__dirname + '/public/404.html')
 })
 
 //Lo que hace es escuchar a traves de que puerto se esta ejecutando 
-//Nos devuelve cual es el puerto
 app.listen(port, () => {
   console.log(`Acceda al servidos haciendo click aqui http://localhost:${port}`)
 })
