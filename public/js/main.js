a6bcd9cafa06393614ce6950523d2e33fdeaafb8
Karen Vargas <kalivargas.11@gmail.com>
2023-03-14 00:31:46 -0500

    Adelantos tema Dark Pagina 1, faltan las otras paginas y organizar codigo

--- public/js/main.js
+++ public/js/main.js
@@ -116,9 +116,30 @@ $(function() {
 		});
 	}; 
 	siteMenuClone();
-
-	    
 });
 
+
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () =>{
+	document.body.classList.toggle('dark');
+	btnSwitch.classList.toggle('active');
+
+	//Guadar el modo en localstorage
+	if(document.body.classList.contains('dark')){
+		localStorage.setItem('dark-mode', 'true')
+	}else{
+		localStorage.setItem('dark-mode', 'false')
+	}
+});
+
+//Obetener el modo actual
+if (localStorage.getItem('dark-mode')=='true') {
+	document.body.classList.add('dark');
+	btnSwitch.classList.add('active');
+}else{
+	document.body.classList.remove('dark');
+	btnSwitch.classList.remove('active');
+}
 
 
